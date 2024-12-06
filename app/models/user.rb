f406c31d3cf589c4d6feb0b84f0685751d5ae0c4
Shinji-ux <shinji.iwasaki1218@gmail.com>
2024-12-06 17:51:15 +0900

    バリデーション解除

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :phone, format: { with: /\A0\d{9,10}\z/, message: "must be あ valid phone number" }
+  # validates :phone, format: { with: /\A0\d{9,10}\z/, message: "must be あ valid phone number" }
 
   has_one :profile
   has_many :categories
