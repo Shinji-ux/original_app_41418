38968848cf2a6df14747c1f10165f2e543e36610
Shinji-ux <shinji.iwasaki1218@gmail.com>
2024-12-10 21:49:56 +0900

    ローカルファイルの読み込みを許可

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -9,12 +9,13 @@ WickedPdf.configure do |config|
                   # デフォルトのオプションを設定
   config.default_options = {
   #   # layout: 'pdf.html',          # レイアウトファイルを指定
-  #   encoding: 'UTF-8',           # エンコーディング
+    encoding: 'UTF-8',           # エンコーディング
   #   page_size: 'A5',             # ページサイズ
   #   # orientation: 'Landscape',    # 横向きに設定
   #   javascript_delay: 1000,      # JavaScriptの実行を待つ時間（ミリ秒）
   #   debug_javascripts: true,     # スクリプトのデバッグ
   #   dpi: 300                     # 出力の解像度
+    enable_local_file_access: true, # ローカルファイルの読み込みを許可
     font_path: '/tmp/fonts/NotoSansCJK-Regular.ttc' # フォントファイルのパス
   }
 end
