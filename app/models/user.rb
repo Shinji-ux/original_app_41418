93f14472b0917007b7c5a7f15f90b2ed1a9d822b
Shinji-ux <shinji.iwasaki1218@gmail.com>
2024-12-11 15:22:01 +0900

    パスワードのバリデーション変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
-  validates_format_of :password, with: PASSWORD_REGEX
+  # PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  # validates_format_of :password, with: PASSWORD_REGEX
   
   validates :phone, format: { with: /\A0\d{9,10}\z/, message: "must be a valid phone number" }
 
